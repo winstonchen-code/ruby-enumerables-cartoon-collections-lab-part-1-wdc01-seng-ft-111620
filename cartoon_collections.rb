7c26a2ad863dfd5285071a3c86b0a68fcf8393f8
Winston Chen <winstonchen.wm@gmail.com>
2020-11-04 06:45:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |title, index|
-    index = 0 
-    while index < array.length do
-      index += 1 
-    end
     puts "#{index} #{title}"
   end
 end
